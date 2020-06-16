ad5754cb2c7b964e64b4cdf5383bee6099b4864f
EricFuma <36584629+EricFuma@users.noreply.github.com>
2020-06-16 23:26:03 +0800

    Update solution.py

--- 算法思想/搜索/Backtracking/5.排列/solution.py
+++ 算法思想/搜索/Backtracking/5.排列/solution.py
@@ -11,10 +11,10 @@ class Solution:
         self.search([], nums, result)
         return result
     
-    def search(self, pre_num: List[int], nums, result) -> None:
+    def search(self, pre_nums: List[int], nums, result) -> None:
         if len(nums) == 0:
-            result.append(pre_num)
+            result.append(pre_nums)
         else:
             # 回溯在这儿，用python中list的特点避免了回退操作，但如果想只用一个list变量，那么仍然要用到append&pop进行回溯
             for i,num in enumerate(nums):
-                self.search(pre_num+[num], nums[:i]+nums[i+1:], result)
+                self.search(pre_nums+[num], nums[:i]+nums[i+1:], result)
