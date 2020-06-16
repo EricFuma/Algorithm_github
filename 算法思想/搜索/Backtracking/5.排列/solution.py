9ba9c4bb77124f8a07435c5f991e908fc5ee303b
EricFuma <36584629+EricFuma@users.noreply.github.com>
2020-06-16 22:56:27 +0800

    Create solution.py

---
+++ 算法思想/搜索/Backtracking/5.排列/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # 特判
+        if not nums:
+            return nums
+
+        # 没有重复
+        # 全排列： n x n-1 x n-2 x ... x 2
+        # 设计一个函数，有一个必要参数是 当前已经获取到的元素
+        result = []
+        self.search([], nums, result)
+        return result
+    
+    def search(self, pre_num: List[int], nums, result) -> None:
+        if len(nums) == 0:
+            result.append(pre_num)
+        else:
+            # 回溯在这儿，用python中list的特点避免了回退操作，但如果想只用一个list变量，那么仍然要用到append&pop进行回溯
+            for i,num in enumerate(nums):
+                self.search(pre_num+[num], nums[:i]+nums[i+1:], result)
