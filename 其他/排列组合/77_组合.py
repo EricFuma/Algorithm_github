ef4bcae84b84c9545ccb0300b0c37a14bfea1cc0
EricFuma <36584629+EricFuma@users.noreply.github.com>
2020-08-24 20:13:11 +0800

    Create 77_组合.py

---
+++ 其他/排列组合/77_组合.py
@@ -0,0 +1,43 @@
+from typing import List
+'''
+不考虑顺序 == 限定死某一种顺序 ==》从小到大
+仍然是回溯法
+'''
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if n <= 0 or n < k or k == 0:
+            return []
+        res = []
+        #nums = [i+1 for i in range(n)]
+        self.search([], 1, k, n, res)
+        return res
+    
+    def search(self, curr_nums, start, k, n, res):
+        if len(curr_nums) == k:
+            res.append(curr_nums[:])   # 切片！ 在回溯设计阶段就用切片变量输入到函数中
+        else:
+            for num in range(start, n+1):
+                curr_nums.append(num)
+                self.search(curr_nums, num+1, k, n, res)
+                # 回溯
+                curr_nums.pop()
+            
+    '''
+    优化版本的search —— 提前停止
+    '''
+    def improve_search(self, curr_nums, start, k, n, res):
+        # 剩余元素已经不够，不用再继续了
+        if n + 1 - start < k - len(curr_nums):
+            return False
+        for num in range(start, n+1):
+            curr_nums.append(num)
+            if len(curr_nums) == k:
+                res.append(curr_nums[:])
+            else:
+                self.improve_search(curr_nums, num+1, k, n, res)
+            # 回溯
+            curr_nums.pop()
+        
+
+            
+
